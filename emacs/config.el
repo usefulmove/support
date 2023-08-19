12353707e45a65f3705e58e87ea7699c648b5745
Duane Edmonds <duane.edmonds@gmail.com>
2023-08-18 20:25:44 -0700

    added terminal ( F8 ) and evaluate S-expression ( F5 ) keybindings

--- emacs/config.el
+++ emacs/config.el
@@ -108,8 +108,10 @@
     (puthash 19 "very doubtful." magic8-hash)
       (message (concat "magic8: "(gethash (random (hash-table-count magic8-hash)) magic8-hash)))))
 
-(global-set-key (kbd "C-c e") 'eros-eval-last-sexp)
 (global-set-key (kbd "C-c t") 'term)
+(global-set-key (kbd "<f8>") 'term)
+(global-set-key (kbd "C-c e") 'eros-eval-last-sexp)
+(global-set-key (kbd "<f5>") 'eros-eval-last-sexp)
 
 (custom-set-faces
   '(rainbow-delimiters-depth-1-face ((t (:foreground "#fffc67"))))
