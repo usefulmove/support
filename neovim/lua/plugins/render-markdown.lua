917bcce1b7f85358e99a2549e26bf61153b02718
Duane Edmonds <duane.edmonds@gmail.com>
2025-11-14 12:20:13 -0800

    added render-markdown plugin

---
+++ neovim/lua/plugins/render-markdown.lua
@@ -0,0 +1,32 @@
+return {
+  'MeanderingProgrammer/render-markdown.nvim',
+  dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-mini/mini.nvim' },
+  opts = {
+    window = {
+      conceal = {
+        spell = true,
+      },
+    },
+    headings = {
+      level = { 1, 2, 3, 4, 5, 6 },
+      border = false,
+      icons = { '󰲡 ', '󰲣 ', '󰲥 ', '󰲧 ', '󰲩 ', '󰲫 ' }, -- keep icons, or set to {} to disable
+    },
+    code = {
+      fence = true,
+    },
+    links = {
+      inline = true,
+    },
+    block_quote = {
+      enabled = true,
+    },
+    link = {
+      hyperlink = '', -- remove hyperlink icon
+    },
+    checkbox = {
+      unchecked = { icon = '_' },
+      checked = { icon = 'x' },
+    },
+  },
+}
